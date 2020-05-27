40ba3f3016267d2f3e831a662df6e3e5afa57990
nitay880 <nitay880@cs.huji.ac.il>
2020-05-28 01:28:24 +0300

    new post request

--- backend/zulu/routes.py
+++ backend/zulu/routes.py
@@ -1,10 +1,8 @@
 from typing import List
-
 from fastapi import APIRouter, Depends, status
 from geojson import Point
-
-from zulu.db_tools import points_db
-from zulu.models import UserLocation, UserLocationResponse, Contributors
+from zulu.db_tools import points_db,story_db
+from zulu.models import UserLocation, UserLocationResponse, UserStory,Contributors,UserStoryResponse
 api = APIRouter()
 
 
@@ -35,3 +33,28 @@ def create_point(location: UserLocation, db=Depends(points_db)):
 @api.get("/contributors", response_model=List[Contributors])
 def get_contributors():
     return ({'name': "nvg", 'github_profile': "https://github.com/nvgoldin"},{'name':"nitai_m", 'github_profile': "https://github.com/Nitay880"})
+
+
+    
+@api.post("/upload_story", status_code=status.HTTP_201_CREATED)
+def create_story(story:UserStory,db=Depends(story_db)):
+    db.insert({"user_location": story.user_location.as_point(), "user_id": story.user_id,"subject":story.subject,"content":story.content})
+    return {}
+
+    """
+    post format: (via postman)
+    {
+    "user_id": "Nitay mordechai @X@",
+    "subject": "I love ya man",
+    "content": "chocolate is nice",
+    "user_location":{"longitude": 0.25,
+                             "latitude": 244}
+    }
+    """
+
+
+#get via /api/all_stories?
+
+@api.get("/all_stories", response_model=List[UserStoryResponse])
+def get_contributor(db=Depends(story_db)):
+    return [s for s in db.find()]
