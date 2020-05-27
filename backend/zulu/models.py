8be5f74c5597842b01c2366a24acf245e99e0cf2
nitay880 <nitay880@cs.huji.ac.il>
2020-05-28 01:26:04 +0300

    new story

--- backend/zulu/models.py
+++ backend/zulu/models.py
@@ -3,23 +3,39 @@ from typing import List
 from geojson import Point
 from pydantic import BaseModel, HttpUrl
 
-
-class UserLocation(BaseModel):
-    user_id: str
+class Location(BaseModel):
     longitude: float
     latitude: float
-
     def as_point(self):
         return Point((self.longitude, self.latitude))
 
+class UserLocation(BaseModel):
+    user_id: str
+    user_location: Location
+
+    def as_point(self):
+        return UserLocation.as_point()
+
+class UserStory(BaseModel):
+    user_id: str
+    user_location: Location
+    subject: str
+    content: str
+    
 
 class GeoJSONPoint(BaseModel):
     type: str = 'Point'
     coordinates: List
 
-
 class UserLocationResponse(BaseModel):
     location: GeoJSONPoint
+    user_id: str
+
+
+class UserStoryResponse(BaseModel):
+    user_location: GeoJSONPoint
+    subject: str
+    content: str
     user_id: str
 
 
