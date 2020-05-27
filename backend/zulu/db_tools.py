6c43a3a0968070d0495fa1270e37c9d45f7ccff5
nitay880 <nitay880@cs.huji.ac.il>
2020-05-28 01:32:20 +0300

    new collection: stories in points db

--- backend/zulu/db_tools.py
+++ backend/zulu/db_tools.py
@@ -1,15 +1,20 @@
 from dynaconf import settings
 from pymongo import GEOSPHERE, MongoClient
 
-
+ # a user references a story which references a point
 def points_db():
     client = MongoClient(settings.MONGO_URI).geo_data
     collection = client.points
-
     collection.create_index([("location", GEOSPHERE)])
     yield collection
-
-
+def story_db():
+    client = MongoClient(settings.MONGO_URI).geo_data
+    collection = client.stories
+    yield collection
+def user_db():
+    client = MongoClient(settings.MONGO_URI).users_data
+    collection = client.stories
+    yield collection
 def init_db():
     client = MongoClient(settings.MONGO_URI).geo_data
     collection = client.points
