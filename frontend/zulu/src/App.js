29b10fda17a3676bb9d6fbf229d195815153ae16
Joshua Herskowitz <joshua.herskowitz@fiverr.com>
2020-06-18 23:26:56 +0300

    get button to show

--- frontend/zulu/src/App.js
+++ frontend/zulu/src/App.js
@@ -2,19 +2,68 @@ import React, {Component} from 'react';
 import logo from './geo_icon.png';
 import './App.css';
 import MapComponent from "./components/Map/MapGeojsonMarkers.jsx";
+import { Container, Button, Link, darkColors} from 'react-floating-action-button'
+import { FaPlus} from 'react-icons/fa';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header" >
-                   <h2>Zulu App</h2>
-                    <img src={logo} className="App-logo" alt="logo"  />
-                </div>
-                <MapComponent/>
+                <Header/>
+                <MapWidow/>
+                <Buttons/>
             </div>
         );
     }
 }
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="App-header" >
+                <h2>Zulu App</h2>
+                <img src={logo} className="App-logo" alt="logo"  />
+            </div>
+        )
+    }
+}
+
+class MapWidow extends Component {
+    render() {
+        return(
+            <div className="">
+                <MapComponent/>
+            </div>
+        )
+    }
+}
+
+class Buttons extends Component {
+    render() {
+        return(
+            <div className="leaflet-bottom leaflet-right buttons-container" >
+                 <Container>
+                    <Button
+                        onClick={() => alert('POST')}
+                        styles={{backgroundColor: darkColors.cyan, color: darkColors.white}}
+                        tooltip="Create note link"
+                        icon={FaPlus} />
+                    <Button
+                        onClick={() => alert('POST')}
+                        styles={{backgroundColor: darkColors.cyan, color: darkColors.white}}
+                        tooltip="Add user link"
+                        icon={FaPlus} />
+                    <Button
+                        styles={{backgroundColor: darkColors.cyan, color: darkColors.white}}
+                        tooltip="Add a new post!"
+                        icon="react-icons/fa"
+                        rotate={true}
+                        onClick={() => alert('POST')} />
+                </Container>
+            </div>
+        )
+    }
+}
+
 
 export default App;
