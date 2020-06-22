c782adb1a38fa2861ed9a4d02f32c65cd43e8fac
Joshua Herskowitz <joshua.herskowitz@fiverr.com>
2020-06-22 21:19:13 +0300

    start working with the modal

--- frontend/zulu/src/App.js
+++ frontend/zulu/src/App.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import logo from './geo_icon.png';
 import './App.css';
 import MapComponent from "./components/Map/MapGeojsonMarkers.jsx";
 import { Container, Button, Link, darkColors} from 'react-floating-action-button'
+import { Modal, Button as OButton} from 'react-bootstrap'
 import { FaPlus} from 'react-icons/fa';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
             <div className="App">
                 <MapWidow/>
                 <Buttons/>
+                <PostModal/>
                 <CornerLogo/>
             </div>
         );
@@ -28,6 +30,11 @@ class MapWidow extends Component {
 }
 
 class Buttons extends Component {
+
+    showModal = () => {this.setState({ show: true });};
+
+    hideModal = () => {this.setState({ show: false });};
+
     render() {
         return(
             <div className="leaflet-bottom leaflet-right buttons-container" >
@@ -47,12 +54,39 @@ class Buttons extends Component {
                         tooltip="Add a new story!"
                         icon="react-icons/fa"
                         rotate={true}
-                        onClick={() => alert('POST')} />
+                        onClick={this.showModal} />
                 </Container>
             </div>
         )
     }
 }
+
+class PostModal extends Component {
+
+    render() {
+        return(
+            <div>
+              <Modal show={true} onHide={null} centered>
+                <Modal.Header closeButton>
+                  <Modal.Title>Post a story</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Enter your post!</Modal.Body>
+                <Modal.Footer>
+                  <OButton variant="primary" onClick={this.hideModal} styles={{backgroundColor: darkColors.cyan, color: darkColors.white}}>
+                    Close
+                  </OButton>
+                  <OButton variant="secondary" onClick={this.hideModal} styles={{backgroundColor: darkColors.cyan, color: darkColors.white}}>
+                    Post
+                  </OButton>
+                </Modal.Footer>
+              </Modal>
+            </div>
+        )
+    }
+}
+
+
+
 
 class CornerLogo extends Component {
     render() {
