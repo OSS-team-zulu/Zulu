2ddea142906456590e2a58cadd91b14205b7b320
nitay <nitay880@gmail.com>
2020-06-30 18:03:41 +0300

    merged wikipedia with protected_login

--- frontend/zulu/src/App.js
+++ frontend/zulu/src/App.js
@@ -2,19 +2,84 @@ import React, {Component} from 'react';
 import logo from './geo_icon.png';
 import './App.css';
 import MapComponent from "./components/Map/MapGeojsonMarkers.jsx";
+import {Buttons, PostModal} from "./components/Map/MapGeojsonMarkers";
 
+import {
+    BrowserRouter ,
+    Switch,
+    Route,
+    Link,Redirect
+} from "react-router-dom";
+
+
+import Login from "./components/LoginForm/Login"
 class App extends Component {
+    state = {
+        loggedIn: false,
+    };
     render() {
         return (
-            <div className="App">
-                <div className="App-header" >
-                   <h2>Zulu App</h2>
-                    <img src={logo} className="App-logo" alt="logo"  />
+            <BrowserRouter>
+                <div>
+                    <CornerLogo/>
+                    <Switch>
+                        <ProtectedRoute exact path='/map' user={""} handleLogout={""} component={MapComponent} />
+                        <Route path="/">
+                            <Login/>
+                        </Route>
+                    </Switch>
                 </div>
-                <MapComponent/>
-            </div>
+            </BrowserRouter>
         );
     }
+
+}
+
+
+class MapWidow extends Component {
+    render() {
+        return(
+            <div className="">
+                <MapComponent/>
+            </div>
+        )
+    }
+}
+class CornerLogo extends Component {
+    render() {
+        return(
+            <div className="leaflet-top  leaflet-left">
+                <div className="App-header" >
+                    <h2>Zulu App</h2>
+                    <img src={logo} className="App-logo" alt="logo"  />
+                </div>
+            </div>
+        )
+    }
+}
+
+
+const ProtectedRoute = ({ component: Component, user, ...rest }) => {
+    return (
+        <Route {...rest} render={
+            props => {
+
+                /*Add authentication test here*/
+                if (true) {
+                    return <Component {...rest} {...props} />
+                } else {
+                    return <Redirect to={
+                        {
+                            pathname: '/',
+                            state: {
+                                from: props.location
+                            }
+                        }
+                    } />
+                }
+            }
+        } />
+    )
 }
 
 export default App;
