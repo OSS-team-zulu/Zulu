99a7858048cceb1ea91f8cfdd1e8f409290ba63f
nitay880 <nitay880@cs.huji.ac.il>
2020-07-05 23:29:14 +0300

    improve wikipedia visibility

--- frontend/zulu/src/components/Map/GeoWikipediaLayerFunc.jsx
+++ frontend/zulu/src/components/Map/GeoWikipediaLayerFunc.jsx
@@ -3,14 +3,23 @@ import {Marker, FeatureGroup, Popup} from 'react-leaflet';
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import Card from '../Card/Card'
 import { iconWiki } from '../../Icons/Icon.Wiki';
+import "./GeoWiki.css";
 
 
 var fetchData = function fetchData(lat, lng, maxDist, options) {
     const url = "https://en.wikipedia.org/w/api.php?action=query&generator=geosearch&prop=coordinates%7C" +
-    "pageimages%7Cextracts&exintro=&ggscoord=" + lat + "%7C" + lng + "&ggsradius=" + maxDist + "&format=json"
+    "pageimages%7Cextracts&exintro=&explaintext=true&ggscoord=" + lat + "%7C" + lng + "&ggsradius=" + maxDist + "&format=json"
     var request = fetch(url, options).then(r => r.json());
     
-    return request.then(r => Object.values(r.query.pages));    
+    return request.then(r =>  {
+
+        if (r.hasOwnProperty("query")){
+            return Object.values(r.query.pages);
+        }
+        else{
+            return [];
+        }
+        });    
 }
 
 
@@ -52,20 +61,30 @@ export default function GeoWikipediaLayer({lat, lng, maxDist, cluster}) {
                     position={[f.coordinates[0].lat, f.coordinates[0].lon]}
                     icon={iconWiki}
                 >
-                    <Popup minWidth={400} closeButton={true} closeOnClick={false} autoClose={false}>
-                        {
-                        <Card background='#2980B9' height="400">
-                            <h1>{f.title}</h1>
-                            { f.hasOwnProperty('thumbnail') ? 
-                                <img src={parseImagePath(f.thumbnail.source)} style={{maxHeight: "430px", maxWidth: "430px"}}></img>
-                                 :
-                                <p></p> 
-                            }
-                            <div dangerouslySetInnerHTML={{__html: f.extract}} />
 
-                        </Card>
-                        }
-                    </Popup>
+                        <Popup minWidth={300}  closeButton={true} closeOnClick={false} autoClose={false}>
+
+                            <Card background='#2980B9' height="200"  >
+                            <div  style={{marginLeft:"auto" ,marginRight:"auto", height: "200px", overflowY: "scroll"} }>
+                            <a href={"https://en.wikipedia.org/?curid="+f.pageid} style={{color:"black",textDecoration:"none"}}>
+                            
+                            <h3>{f.title}</h3>
+                            </a>
+                            { f.hasOwnProperty('thumbnail') ? 
+                                <img display="inline-block" src={parseImagePath(f.thumbnail.source)} style={{maxHeight: "200px", maxWidth: "200px"}}></img>
+                                 :
+                                <p ></p> 
+                            }
+
+<p id="desc" style={{float:"left",dir:"rtl",color: "black",fontWeight: "bold" ,lineHeight: "18px"}}>
+
+{f.extract}
+</p>
+</div>
+
+                            </Card>
+
+                    </Popup>    
 
 
                 </Marker>
