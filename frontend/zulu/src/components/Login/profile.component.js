a161206cf91eac37868e952faa1b7c0fd52c5410
Roy Ackerman <roy.akerman@mail.huji.ac.il>
2020-07-07 16:33:51 +0300

    Profile page now supports updating passwords by the user + About Me section.

--- frontend/zulu/src/components/Login/profile.component.js
+++ frontend/zulu/src/components/Login/profile.component.js
@@ -8,6 +8,27 @@ export default class Profile extends Component {
     this.state = { currentUser: AuthService.getCurrentUser() };
 
   }
+
+
+  onChangePassword(e) {
+    this.state = {
+      currentUser: {
+        password: e.target.value
+      }
+    }
+  }
+
+  vpassword = value => {
+    if (value.length < 6 || value.length > 40) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          The password must be between 6 and 40 characters.
+        </div>
+      );
+    }
+  };
+
+
   render() {
     console.log(this.state.currentUser);
     return (
@@ -19,12 +40,33 @@ export default class Profile extends Component {
         </header>
         <p>
           <strong>Full name:</strong>{" "}
-          {this.state.currentUser.full_name}
+          {this.state.currentUser.fullname}
         </p>
         <p>
           <strong>Email:</strong>{" "}
           {this.state.currentUser.email}
         </p>
+
+        <div className="form-group">
+                  <label htmlFor="password">Update Password:</label>
+                  <Input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value="New password"
+                    onChange={this.onChangePassword}
+                    validations={[required, vpassword]}
+                  />
+        </div>
+
+        <p>
+          <strong>About Me:</strong>{" "}
+          {this.state.currentUser.fullname} ! What can you tell us about you?
+        </p>
+
+
+
+
       </div>
     );
   }
