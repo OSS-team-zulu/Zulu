358f9775c0e66ba2fc56c767b4313617b1ef0f6e
Rom Zats <rom@fruitspec.com>
2020-07-09 09:57:19 +0300

    small grammer fix

--- frontend/zulu/src/components/Login/register.component.js
+++ frontend/zulu/src/components/Login/register.component.js
@@ -1,18 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
-import {isEmail} from "validator";
-import {isAlpha} from "validator";
+import { isEmail } from "validator";
+import { isAlpha } from "validator";
 
 import AuthService from "../../services/auth.service";
 
 const required = value => {
   if (!value) {
     return (
-        <div className="alert alert-danger" role="alert">
-          This field is required!
-        </div>
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
     );
   }
 };
@@ -20,9 +20,9 @@ const required = value => {
 const email = value => {
   if (!isEmail(value)) {
     return (
-        <div className="alert alert-danger" role="alert">
-          This is not a valid email.
-        </div>
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
     );
   }
 };
@@ -31,9 +31,9 @@ const email = value => {
 const vusername = value => {
   if (value.length < 3 || value.length > 20 || !isAlpha(value)) {
     return (
-        <div className="alert alert-danger" role="alert">
-          The full name must be between 3 and 20 characters, and only letters are allowed.
-        </div>
+      <div className="alert alert-danger" role="alert">
+        The full name must be between 3 and 20 characters, and only letters are allowed.
+      </div>
     );
   }
 };
@@ -42,20 +42,21 @@ const vusername = value => {
 const vfullname = value => {
   if (value.length < 3 || value.length > 20) {
     return (
-        <div className="alert alert-danger" role="alert">
-          The username must be between 3 and 20 characters.
-        </div>
+      <div className="alert alert-danger" role="alert">
+        The username must be between 3 and 20 characters.
+      </div>
     );
   }
 };
 
 
+
 const vpassword = value => {
   if (value.length < 6 || value.length > 40) {
     return (
-        <div className="alert alert-danger" role="alert">
-          The password must be between 6 and 40 characters.
-        </div>
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
     );
   }
 };
@@ -68,7 +69,6 @@ export default class Register extends Component {
     this.onChangeFullname = this.onChangeFullname.bind(this);
     this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
-    this.onChangeAboutMe = this.onChangeAboutMe.bind(this);
 
     this.state = {
       username: "",
@@ -76,7 +76,6 @@ export default class Register extends Component {
       password: "",
       successful: false,
       fullName: "",
-      aboutMe: "",
       message: ""
     };
   }
@@ -106,12 +105,6 @@ export default class Register extends Component {
     });
   }
 
-  onChangeAboutMe(e) {
-    this.setState({
-      aboutMe: e.target.value
-    });
-  }
-
   handleRegister(e) {
     e.preventDefault();
 
@@ -124,144 +117,135 @@ export default class Register extends Component {
 
     if (this.checkBtn.context._errors.length === 0) {
       AuthService.register(
-          this.state.username,
-          this.state.email,
-          this.state.password,
-          this.state.fullName,
-          this.state.aboutMe
+        this.state.username,
+        this.state.email,
+        this.state.password,
+        this.state.fullName
       ).then(
-          response => {
-            this.setState({
-              message: response.data.message,
-              successful: true
-            });
-            this.props.history.push("/login");
-          },
-          error => {
-            const resMessage =
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString();
+        response => {
+          this.setState({
+            message: response.data.message,
+            successful: true
+          });
+          this.props.history.push("/login");
+        },
+        error => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
 
-            this.setState({
-              successful: false,
-              message: resMessage
-            });
-          }
+          this.setState({
+            successful: false,
+            message: resMessage
+          });
+        }
       );
     }
   }
 
   render() {
     return (
-        <div className="col-md-12">
-          <div className="card card-container">
-            <img
-                src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                alt="profile-img"
-                className="profile-img-card"
+      <div className="col-md-12">
+        <div className="card card-container">
+          <img
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="profile-img"
+            className="profile-img-card"
+          />
+
+          <Form
+            onSubmit={this.handleRegister}
+            ref={c => {
+              this.form = c;
+            }}
+          >
+            {!this.state.successful && (
+              <div>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    validations={[required, vusername]}
+                  />
+                </div>
+
+
+
+                <div className="form-group">
+                  <label htmlFor="fullname">Full name</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="fullname"
+                    value={this.state.fullname}
+                    onChange={this.onChangeFullname}
+                    validations={[required, vfullname]}
+                  />
+                </div>
+
+
+
+
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChangeEmail}
+                    validations={[required, email]}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                    validations={[required, vpassword]}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <button className="btn btn-primary btn-block">Sign Up</button>
+                </div>
+              </div>
+            )}
+
+            {this.state.message && (
+              <div className="form-group">
+                <div
+                  className={
+                    this.state.successful
+                      ? "alert alert-success"
+                      : "alert alert-danger"
+                  }
+                  role="alert"
+                >
+                  {this.state.message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              style={{ display: "none" }}
+              ref={c => {
+                this.checkBtn = c;
+              }}
             />
-
-            <Form
-                onSubmit={this.handleRegister}
-                ref={c => {
-                  this.form = c;
-                }}
-            >
-              {!this.state.successful && (
-                  <div>
-                    <div className="form-group">
-                      <label htmlFor="username">Username</label>
-                      <Input
-                          type="text"
-                          className="form-control"
-                          name="username"
-                          value={this.state.username}
-                          onChange={this.onChangeUsername}
-                          validations={[required, vusername]}
-                      />
-                    </div>
-
-
-                    <div className="form-group">
-                      <label htmlFor="fullname">Full name</label>
-                      <Input
-                          type="text"
-                          className="form-control"
-                          name="fullname"
-                          value={this.state.fullname}
-                          onChange={this.onChangeFullname}
-                          validations={[required, vfullname]}
-                      />
-                    </div>
-
-
-                    <div className="form-group">
-                      <label htmlFor="email">Email</label>
-                      <Input
-                          type="text"
-                          className="form-control"
-                          name="email"
-                          value={this.state.email}
-                          onChange={this.onChangeEmail}
-                          validations={[required, email]}
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="password">Password</label>
-                      <Input
-                          type="password"
-                          className="form-control"
-                          name="password"
-                          value={this.state.password}
-                          onChange={this.onChangePassword}
-                          validations={[required, vpassword]}
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="aboutMe">About me</label>
-                      <textarea
-                          rows="3"
-                          className="form-control"
-                          name="aboutMe"
-                          value={this.state.aboutMe}
-                          onChange={this.onChangeAboutMe}
-                      />
-                    </div>
-
-                    <div className="form-group">
-                      <button className="btn btn-primary btn-block">Sign Up</button>
-                    </div>
-                  </div>
-              )}
-
-              {this.state.message && (
-                  <div className="form-group">
-                    <div
-                        className={
-                          this.state.successful
-                              ? "alert alert-success"
-                              : "alert alert-danger"
-                        }
-                        role="alert"
-                    >
-                      {this.state.message}
-                    </div>
-                  </div>
-              )}
-              <CheckButton
-                  style={{display: "none"}}
-                  ref={c => {
-                    this.checkBtn = c;
-                  }}
-              />
-            </Form>
-          </div>
+          </Form>
         </div>
+      </div>
     );
   }
 }
